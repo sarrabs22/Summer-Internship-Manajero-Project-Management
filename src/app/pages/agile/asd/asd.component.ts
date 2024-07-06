3670e5b9846484b46ca2fad368ed4ee5312c076b
sarrabs22 <sarra.bensedrine@esprit.tn>
2024-07-06 19:43:43 +0100

    version_static

--- src/app/pages/agile/asd/asd.component.ts
+++ src/app/pages/agile/asd/asd.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-asd',
   templateUrl: './asd.component.html',
-  styleUrls: ['./asd.component.css']
+  styleUrls: ['./asd.component.scss']
 })
 export class AsdComponent implements OnInit {
 
-  constructor() { }
+  firstForm: UntypedFormGroup;
+  secondForm: UntypedFormGroup;
+  thirdForm: UntypedFormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: UntypedFormBuilder) {
   }
 
+  ngOnInit() {
+    this.firstForm = this.fb.group({
+      firstCtrl: ['', Validators.required],
+    });
+
+    this.secondForm = this.fb.group({
+      secondCtrl: ['', Validators.required],
+    });
+
+    this.thirdForm = this.fb.group({
+      thirdCtrl: ['', Validators.required],
+    });
+  }
+
+  onFirstSubmit() {
+    this.firstForm.markAsDirty();
+  }
+
+  onSecondSubmit() {
+    this.secondForm.markAsDirty();
+  }
+
+  onThirdSubmit() {
+    this.thirdForm.markAsDirty();
+  }
 }
