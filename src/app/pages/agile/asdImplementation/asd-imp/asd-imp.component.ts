692f3fb2a4a4eb226a36ab432dabfced9c567ed7
Hamza Tounsi <tounsi.hamza@esprit.tn>
2024-07-12 01:01:30 +0100

    manajero

--- src/app/pages/agile/asdImplementation/asd-imp/asd-imp.component.ts
+++ src/app/pages/agile/asdImplementation/asd-imp/asd-imp.component.ts
@@ -26,62 +26,63 @@ export class  AsdImpComponent{
   constructor(private http: HttpClient) { }
 
   createUser() {
-    this.http.post('/api/users', this.newUser)
+    this.http.post('http://localhost:9090/api/users', this.newUser)
       .subscribe(response => {
         console.log('User created:', response);
         this.newUser = {};
       });
   }
-
+  
   getUser() {
-    this.http.get(`/api/users/${this.userId}`)
+    this.http.get(`http://localhost:9090/api/users/${this.userId}`)
       .subscribe(response => {
         this.user = response;
       });
   }
-
+  
   createProject() {
-    this.http.post('/api/projects', this.newProject)
+    this.http.post('http://localhost:9090/api/projects', this.newProject)
       .subscribe(response => {
         console.log('Project created:', response);
         this.newProject = {};
       });
   }
-
+  
   getProject() {
-    this.http.get(`/api/projects/${this.projectId}`)
+    this.http.get(`http://localhost:9090/api/projects/${this.projectId}`)
       .subscribe(response => {
         this.project = response;
       });
   }
-
+  
   createTask() {
-    this.http.post('/api/tasks', this.newTask)
+    this.http.post('http://localhost:9090/api/tasks', this.newTask)
       .subscribe(response => {
         console.log('Task created:', response);
         this.newTask = {};
       });
   }
-
+  
   getTask() {
-    this.http.get(`/api/tasks/${this.taskId}`)
+    this.http.get(`http://localhost:9090/api/tasks/${this.taskId}`)
       .subscribe(response => {
         this.task = response;
       });
   }
-
+  
   createFeedback() {
-    this.http.post('/api/feedbacks', this.newFeedback)
+    this.http.post('http://localhost:9090/api/feedbacks', this.newFeedback)
       .subscribe(response => {
         console.log('Feedback created:', response);
         this.newFeedback = {};
       });
   }
-
+  
   getFeedback() {
-    this.http.get(`/api/feedbacks/${this.feedbackId}`)
+    this.http.get(`http://localhost:9090/api/feedbacks/${this.feedbackId}`)
       .subscribe(response => {
         this.feedback = response;
       });
   }
+  
 }
